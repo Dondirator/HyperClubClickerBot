a43166351ee04e9c267874dd56f88e33ffcfdcd0
Dondirator <75539044+Dondirator@users.noreply.github.com>
2024-08-22 13:08:24 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { 
+    
+}
