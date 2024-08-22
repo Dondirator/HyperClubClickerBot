085c79d26e4c6ac6d9d8fcdd7e204266ea8bf361
Dondirator <75539044+Dondirator@users.noreply.github.com>
2024-08-22 15:11:41 +0300

    123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
