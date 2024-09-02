b8352eb6e5d9252ef825ae6c93e5bf70786cedaa
Dondirator <75539044+Dondirator@users.noreply.github.com>
2024-09-02 13:42:34 +0300

    123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { EventsComponent } from './pages/events/events.component';
 import { EventAtTonComponent } from './pages/event-at-ton/event-at-ton.component';
+import { GuideComponent } from './pages/guide/guide.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'events', component: EventsComponent},
   {path: 'event-at-ton', component: EventAtTonComponent},
+  {path: 'guide', component: GuideComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EventItemComponent } from './components/event-item/event-item.component
 import { BlurredCircleComponent } from './components/blurred-circle/blurred-circle.component';
 import { EventAtTonComponent } from './pages/event-at-ton/event-at-ton.component';
 import { TonEventItemComponent } from './components/ton-event-item/ton-event-item.component';
+import { GuideComponent } from './pages/guide/guide.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { TonEventItemComponent } from './components/ton-event-item/ton-event-ite
     BlurredCircleComponent,
     EventAtTonComponent,
     TonEventItemComponent,
+    GuideComponent,
   ],
   imports: [
     BrowserModule,
